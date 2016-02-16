54832453bbbb0735a97f7ebec67d02978693d2ce
wfx <wolfgang.morawetz@gmail.com>
2016-02-16 22:03:46 +0100

    use now state to control the client.

--- server.js
+++ server.js
@@ -37,48 +37,15 @@ io.on('connection', function(socket) {
         console.log(
           "help : show this \n" +
           "quit : will kill youre kitty\n" +
-          "continent [CONTINENT] : It shows info about the continent or all\n" +
-          "country [COUNTRY]: It shows info about the country or all\n" +
-          "place [TYPE] [AMOUNT] [COUNTRY]: place figure\n" +
-          "remove [TYPE] [AMOUNT] [COUNTRY]: remove figure\n" +
-          "move [COUNTRY] [COUNTRY] [TYPE] [AMOUNT]: move figure"
+          "state [STATE] [DATA]: trigger a state\n"
         );
         break;
       case "quit":
         console.log("...going to kill youre kitty!");
         process.exit(0);
         break;
-      case "continents":
-        socket.emit("continents", {
-          id: cmd[1]
-        });
-        break;
-      case "country":
-        socket.emit("country", {
-          id: cmd[1]
-        });
-        break;
-      case "place":
-        socket.emit("place", {
-          id: cmd[1],
-          type: cmd[2],
-          amount: cmd[3]
-        });
-        break;
-      case "remove":
-        socket.emit("remove", {
-          id: cmd[1],
-          type: cmd[2],
-          amount: cmd[3]
-        });
-        break;
-      case "move":
-        socket.emit("move", {
-          idFrom: cmd[1],
-          idTo: cmd[2],
-          type: cmd[3],
-          amount: cmd[4]
-        });
+      case "state":
+        socket.emit("state", cmd[1], cmd[2]);
         break;
       default:
         console.log("SERVER: command " + line.trim() + " unknow");
@@ -94,7 +61,7 @@ io.on('connection', function(socket) {
     console.log('client disconnected.');
   });
   /*
-  STATES: ..... _________________________________
+  TEG States.....................................
   start: ...... to start the game
   status: ..... shows the status of the players
   message: .... to send a message
@@ -119,109 +86,18 @@ io.on('connection', function(socket) {
   options: .... to set options
   robot: ...... to play with a robot
   typeofgame:.. to know the type of game
-  error ......... for any error's
+  error ....... for any error's
   */
-  socket.on("start", function(data) {
-    console.log("receive start:\n" + data);
+
+  socket.on("receive", function(data) {
+    console.log("receive data:\n" + data);
   });
 
-  socket.on("socket", function(data) {
-    console.log("receive socket:\n" + data);
-  });
-
-  socket.on("status", function(data) {
-    console.log("receive status:\n" + data);
-  });
-
-  socket.on("message", function(data) {
-    console.log("receive message:\n" + data);
-  });
-
-  socket.on("exit", function(data) {
-    console.log("receive exit:\n" + data);
-  });
-
-  socket.on("cversion", function(data) {
-    console.log("receive cversion:\n" + data);
-  });
-
-  socket.on("sversion", function(data) {
-    console.log("receive sversion:\n" + data);
-  });
-
-  socket.on("pversion", function(data) {
-    console.log("receive pversion:\n" + data);
-  });
-
-  socket.on("playerID", function(data) {
-    console.log("receive playerID:\n" + data);
-  });
-
-  socket.on("help", function(data) {
-    console.log("receive help:\n" + data);
-  });
-
-  socket.on("continent", function(data) {
-    console.log("receive continent:\n" + data);
-  });
-
-  socket.on("country", function(data) {
-    console.log("receive country:\n" + data);
-  });
-
-  socket.on("place", function(data) {
-    console.log("receive place:\n" + data);
-  });
-
-  socket.on("remove", function(data) {
-    console.log("receive remove:\n" + data);
-  });
-
-  socket.on("move", function(data) {
-    console.log("receive move:\n" + data);
-  });
-
-  socket.on("attac", function(data) {
-    console.log("receive attac:\n" + data);
-  });
-
-  socket.on("turn", function(data) {
-    console.log("receive turn:\n" + data);
-  });
-
-  socket.on("exchange", function(data) {
-    console.log("receive exchange:\n" + data);
-  });
-
-  socket.on("mission", function(data) {
-    console.log("receive mission:\n" + data);
-  });
-
-  socket.on("color", function(data) {
-    console.log("receive color:\n" + data);
+  socket.on("tarzan", function(data) {
+    console.log("tarzan: " + data);
   });
 
   socket.on("echo", function(data) {
-    console.log("receive echo:\n" + data);
-  });
-
-  socket.on("surrender", function(data) {
-    console.log("receive surrender:\n" + data);
-  });
-
-  socket.on("options", function(data) {
-    console.log("receive options:\n" + data);
-  });
-
-  socket.on("robot", function(data) {
-    console.log("receive robot:\n" + data);
-  });
-
-  socket.on("typeofgame", function(data) {
-    console.log("receive typeofgame:\n" + data);
-  });
-
-  socket.on("error", function(data) {
-    u.cout("receive error: %s", data);
+    console.log("echo: " + data);
   });
 });
