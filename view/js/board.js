6d1d354adcd6b5963a4e46065b493d0aee129691
wfx <wolfgang.morawetz@gmail.com>
2016-02-16 22:06:35 +0100

    cleaning

--- view/js/board.js
+++ view/js/board.js
@@ -1,60 +1,48 @@
-function Board(options) {
+function Board(opt) {
   "use strict";
   var self = this,
-    area = {},
-    field = {},
-    file = options.file,
-    viewbox = options.viewbox,
-    surface = new Snap(options.element);
+    map = {},
+    viewbox = opt.viewbox;
 
-  // load the svg and fill up all map area's and field's
-  Snap.load(options.file, function(map) {
-    // TODO: get it from the map.
-    surface.attr({
-      viewBox: options.viewBox
+  self.area = [];
+  self.field = [];
+  self.surface = new Snap(opt.element);
+  self.group = self.surface.group();
+
+  // load the svg and fill up all board area's and field's
+  Snap.load(opt.file, function(map) {
+    // TODO: get it from the map?
+    self.surface.attr({
+      viewBox: opt.viewBox
     });
-    var aid = "", // index var's for area and field.... :/
+    var aid = "",
       fid = "";
     map.selectAll("g").forEach(function(el) {
       if (el.node.attributes["board:area"]) {
         aid = el.node.attributes["board:area"].value.replace(/ /g, "");
-        area[aid] = new Area();
-        area[aid].uID = aid;
-        area[aid].name = el.node.attributes["board:area"].value;
-        area[aid].quality = el.node.attributes["board:quality"].value;
+        self.area[aid] = new Area();
+        self.area[aid].uID = aid;
+        self.area[aid].name = el.node.attributes["board:area"].value;
+        self.area[aid].quality = el.node.attributes["board:quality"].value;
       }
       if (el.node.attributes["board:field"]) {
-        fid = area[aid].uID + el.node.attributes["board:field"].value.replace(/ /g, "");
-        field[fid] = new Field(options.field);
-        area[aid].field = fid;
-        field[fid].uID = fid;
-        field[fid].name = el.node.attributes["board:field"].value;
-        field[fid].boundary = el.node.attributes["board:boundary"].value.split(",");
-        field[fid].areaID = area[aid].uID;
+        fid = self.area[aid].uID + el.node.attributes["board:field"].value.replace(/ /g, "");
+        self.field[fid] = new Field(opt.field);
+        self.area[aid].field = fid;
+        self.field[fid].uID = fid;
+        self.field[fid].name = el.node.attributes["board:field"].value;
+        self.field[fid].boundary = el.node.attributes["board:boundary"].value.split(",");
+        self.field[fid].areaID = self.area[aid].uID;
         el.selectAll("path").forEach(function(el) {
-          field[fid].image = el;
-          // TODO: is bind right used?
-          field[fid].image.hover(field[fid].onHoverOver, field[fid].onHoverOut);
-          field[fid].image.click(field[fid].onClick);
+          self.field[fid].image = el;
+          self.field[fid].image.hover(self.field[fid].onHoverOver, self.field[fid].onHoverOut);
+          self.field[fid].image.click(self.field[fid].onClick);
+          self.field[fid].image.mousemove(self.field[fid].onMouseMove);
         });
       }
     });
-    surface.append(map);
+    self.group.add(map);
   });
-
-  self.getField = function(uID){
-    if (uID) {
-      return {
-        id: uID,
-        boundary: field[uID].boundary,
-        areaID: field[uID].areaID,
-        figure: field[uID].figure,
-        quality: field[uID].quality,
-        selected: field[uID].selected
-      };
-    }
-    return false;
-  };
 
 }
 
@@ -68,7 +56,7 @@ function Area() {
   self.image = {};
 }
 
-function Field(options) {
+function Field(opt) {
   "use strict";
   var self = this;
   self.uID = "";
@@ -79,36 +67,31 @@ function Field(options) {
   self.quality = 0;
   self.image = {};
   self.selected = false;
-  var clicked = options.callbacks.clicked,
-    hoverover = options.callbacks.hoverover,
-    hoverout = options.callbacks.hoverout;
+  var clicked = opt.callbacks.clicked,
+    hoverover = opt.callbacks.hoverover,
+    hoverout = opt.callbacks.hoverout,
+    mousemove = opt.callbacks.mousemove;
 
-  options.figures.forEach(function(t) {
+  opt.figures.forEach(function(t) {
     self.figure[t] = {
       owner: "",
       amount: ""
     };
   });
 
-  var retData = function(){
-    return {
-        id: self.uID,
-        boundary: self.boundary,
-        areaID: self.areaID,
-        figure: self.figure,
-        quality: self.quality,
-        selected: self.selected
-    };
+  self.onClick = function() {
+    clicked(self.uID);
+  };
+  
+  self.onHoverOver = function() {
+    hoverover(self.uID);
   };
 
-  self.onClick = function() {
-    //clicked(self);
-    clicked(retData());
-  };
-  self.onHoverOver = function() {
-    hoverover(retData());
-  };
   self.onHoverOut = function() {
-    hoverout(retData());
+    hoverout(self.uID);
+  };
+
+  self.onMouseMove = function() {
+    mousemove(self.uID);
   };
 }
