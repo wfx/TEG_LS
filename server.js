908cb997235e0fdb5c95d84f24bb222df9d6f826
wfx <wolfgang.morawetz@gmail.com>
2016-03-05 14:04:00 +0100

    cookie work

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 var express = require("express"),
     http = require("http"),
     socketIO = require("socket.io"),
-    conf = require("./config.json");
-    msg = require("./modules/messages.js");
+    conf = require("./config.json"),
+    msg = require("./modules/messages.js"),
+    MemoryStore = express.session.MemoryStore,
+    session_key = 'express.sid',
+    session_secret = 'for signed cookies',
+    session_store = new MemoryStore(),
+    cookieParser = express.cookieParser(session_secret);
 
 /* Init webserver */
 var app = express(),
-    server = http.createServer(app)
+    server = http.createServer(app),
     io = socketIO.listen(server);
 
 server.listen(conf.port);
+
+app.use(cookieParser);
+app.use(express.session({
+  secret: session_secret,
+  store: session_store,
+  key: session_key
+}));
 
 app.use(express.static(__dirname + "/view"));
 app.get('/', function(req, res) {
